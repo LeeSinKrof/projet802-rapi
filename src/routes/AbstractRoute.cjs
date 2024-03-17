d209b5d010fe5285d90e2f807ad5cbe82e554888
LeeSinKrof <ceylan.emre@laposte.net>
2024-03-17 12:50:22 +0100

    delete inutile file

--- src/routes/AbstractRoute.cjs
+++
@@ -1,7 +0,0 @@
-class AbstractRoute {
-    async run(req, reply) {
-        throw new Error('Abstract method run must be implemented in derived classes');
-    }
-}
-
-module.exports = AbstractRoute;
